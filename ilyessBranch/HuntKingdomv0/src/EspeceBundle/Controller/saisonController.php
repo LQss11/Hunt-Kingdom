19434033674de0a5e0b5d6a802e9f53742df763c
IlyessAouini <mohamedilyess.aouini@esprit.tn>
2020-02-24 17:05:00 +0100

    last version for integration

---
+++ ilyessBranch/HuntKingdomv0/src/EspeceBundle/Controller/saisonController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace EspeceBundle\Controller;
+
+use EspeceBundle\Entity\saison;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Saison controller.
+ *
+ */
+class saisonController extends Controller
+{
+    /**
+     * Lists all saison entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $saisons = $em->getRepository('EspeceBundle:saison')->findAll();
+
+        return $this->render('saison/index.html.twig', array(
+            'saisons' => $saisons,
+        ));
+    }
+
+    /**
+     * Creates a new saison entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $saison = new Saison();
+        $form = $this->createForm('EspeceBundle\Form\saisonType', $saison);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($saison);
+            $em->flush();
+
+            return $this->redirectToRoute('saison_show', array('idSaison' => $saison->getIdsaison()));
+        }
+
+        return $this->render('saison/new.html.twig', array(
+            'saison' => $saison,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a saison entity.
+     *
+     */
+    public function showAction(Request $request,saison $saison)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $saison = $em->getRepository('EspeceBundle:saison')->findAll();
+
+        /**
+         * @var $paginator \Knp\Component\Pager\Paginator
+         */
+        $paginator =$this->get('knp_paginator');
+        $result = $paginator->paginate(
+            $saison,
+            $request->query->getInt('page',1),
+            $request->query->getInt('limit',1)
+
+        );
+
+
+        return $this->render('saison/show.html.twig', array(
+            'saison' => $result,
+
+        ));    }
+
+    /**
+     * Displays a form to edit an existing saison entity.
+     *
+     */
+    public function editAction(Request $request, saison $saison)
+    {
+        $deleteForm = $this->createDeleteForm($saison);
+        $editForm = $this->createForm('EspeceBundle\Form\saisonType', $saison);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('saison_edit', array('idSaison' => $saison->getIdsaison()));
+        }
+
+        return $this->render('saison/edit.html.twig', array(
+            'saison' => $saison,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a saison entity.
+     *
+     */
+    public function deleteAction(Request $request, saison $saison)
+    {
+        $form = $this->createDeleteForm($saison);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($saison);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('saison_index');
+    }
+
+    /**
+     * Creates a form to delete a saison entity.
+     *
+     * @param saison $saison The saison entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(saison $saison)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('saison_delete', array('idSaison' => $saison->getIdsaison())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
