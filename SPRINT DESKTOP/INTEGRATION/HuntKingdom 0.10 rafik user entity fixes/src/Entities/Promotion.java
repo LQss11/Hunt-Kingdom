c4dca8feb74bc00acf459bbb47f0ea1480b42208
LQss11 <49565791+LQss11@users.noreply.github.com>
2020-04-21 20:00:14 +0200

    -1

---
+++ SPRINT DESKTOP/INTEGRATION/HuntKingdom 0.10 rafik user entity fixes/src/Entities/Promotion.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+import java.sql.Date;
+/**
+ *
+ * @author khalil
+ */
+public class Promotion {
+     private int id ;
+    private int pourcentage;
+    private Produit Produit;
+    private Date dateFin;
+    private Date dateDebut;
+    private float prix;
+    private int active;
+
+    public Promotion(int pourcentage, Produit Produit, Date dateFin, Date dateDebut,  int active) {
+        this.pourcentage = pourcentage;
+        this.Produit = Produit;
+        this.dateFin = dateFin;
+        this.dateDebut = dateDebut;
+        this.active = active;
+    }
+  
+
+    public Promotion(int id, int pourcentage, Produit Produit, Date dateFin, Date dateDebut) {
+        this.id = id;
+        this.pourcentage = pourcentage;
+        this.Produit = Produit;
+        this.dateFin = dateFin;
+        this.dateDebut = dateDebut;
+    }
+
+    public Promotion(int id, int pourcentage, Date dateFin, Date dateDebut, int active,Produit p) {
+        this.id = id;
+        this.pourcentage = pourcentage;
+        this.dateFin = dateFin;
+        this.dateDebut = dateDebut;
+        this.active = active;
+    
+        this.Produit=p;
+    }
+
+    public Promotion(int id, int pourcentage, Produit Produit, Date dateFin, Date dateDebut, float prix, int active) {
+        this.id = id;
+        this.pourcentage = pourcentage;
+        this.Produit = Produit;
+        this.dateFin = dateFin;
+        this.dateDebut = dateDebut;
+        this.prix = prix;
+        this.active = active;
+    }
+
+    public Promotion(int id, int pourcentage, Produit Produit, Date dateFin, Date dateDebut, float prix) {
+        this.id = id;
+        this.pourcentage = pourcentage;
+        this.Produit = Produit;
+        this.dateFin = dateFin;
+        this.dateDebut = dateDebut;
+        this.prix = prix;
+    }
+
+    public Promotion() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPourcentage() {
+        return pourcentage;
+    }
+
+    public void setPourcentage(int pourcentage) {
+        this.pourcentage = pourcentage;
+    }
+
+    public Produit getProduit() {
+        return Produit;
+    }
+
+    public void setProduit(Produit Produit) {
+        this.Produit = Produit;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" + "id=" + id + ", pourcentage=" + pourcentage + ", Produit=" + Produit + ", dateFin=" + dateFin + ", dateDebut=" + dateDebut + ", prix=" + prix + ", active=" + active + '}';
+    }
+
+    
+}
---
+++ SPRINT DESKTOP/INTEGRATION/HuntKingdom 0.9 interface fixes + integration skander/src/Controllers/PanierController.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import Entities.Produit;
+import Service.ServiceUtilisateursAdresses;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author skand
+ */
+public class PanierController implements Initializable {
+
+    @FXML
+    private JFXListView<String> lp;
+    @FXML
+    private VBox det;
+    @FXML
+    private Label tot;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        hunt.Hunt.listeprod.forEach((t) -> {
+            String str = t.getNom() + " " + t.getPrix() + " $";
+            lp.getItems().add(str);
+        });
+
+        lp.setOnMouseClicked((event) -> {
+            aff();
+        });
+        Double t = hunt.Hunt.listeprod.stream().mapToDouble((p) -> {
+            return p.getPrix();
+        }).sum();
+        tot.setText(String.valueOf(t) + " $");
+    }
+
+    private void aff() {
+        
+        det.getChildren().clear();
+        Produit p = hunt.Hunt.listeprod.get(lp.getSelectionModel().getSelectedIndex());
+        String str="file:"+System.getProperty("user.dir")+"/src/FXML/images/"+p.getImage();
+        ImageView imageView = ImageViewBuilder.create()
+                .image(new Image(str))
+                .build();
+        imageView.setFitWidth(200);
+        imageView.setFitHeight(200);
+        JFXButton btn = new JFXButton("Supprimer");
+
+        btn.setOnAction((event) -> {
+            hunt.Hunt.listeprod.remove(p);
+            lp.getItems().remove(p.getNom() + " " + p.getPrix() + " $");
+            det.getChildren().clear();
+            if (hunt.Hunt.listeprod.size() == 0) {
+                Parent root;
+                //  try {
+                // root = (ScrollPane) FXMLLoader.load(getClass().getResource("/Views/store.fxml"));
+
+                //pijava.pijava.getStage().getScene().setRoot(root);
+                // } catch (IOException ex) {
+                //Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                // }
+            }
+        });
+        btn.setStyle("-fx-background-color:#000000; -fx-text-fill: white;");
+        btn.setPrefWidth(200);
+        det.getChildren().add(imageView);
+        det.getChildren().add(btn);
+    }
+
+    @FXML
+    private void Passercmd(ActionEvent event) throws IOException {
+
+        Stage stage = new Stage();
+        stage.setTitle("adresse livraison");
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/adresselivraison.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
