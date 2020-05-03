44d1c16be06d9d68c15332e73dc4ef9b40e41b55
LQss11 <49565791+LQss11@users.noreply.github.com>
2020-05-03 10:43:59 +0200

    Login

---
+++ SPRINT MOBILE/Salem Branch/0.3 User Login/HuntKingdom/src/com/mycompany/myapp/gui/ListFeedbackForm.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.gui;
+
+import com.codename1.components.SpanLabel;
+import com.codename1.ui.Button;
+import com.codename1.ui.Command;
+import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.Label;
+import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.FlowLayout;
+import com.mycompany.myapp.entities.Feedback;
+import com.mycompany.myapp.services.ServiceFeedback;
+
+/**
+ *
+ * @author LQss
+ */
+public class ListFeedbackForm extends Form {
+
+    Form current;
+
+    public ListFeedbackForm(Form previous) {
+        current = this;
+
+        setTitle("List Feedback");
+        for (Feedback f : ServiceFeedback.getInstance().getAllFeedbacks()) {
+
+            Container cnt1 = new Container(BoxLayout.y());
+
+            Label lbId = new Label(" iD= " + f.getId());
+
+            SpanLabel lbDesc = new SpanLabel(" Desc= " + f.getDescription());
+            //lbDesc.setAutoSizeMode(true);
+            Label lbDate = new Label(" Date= " + f.getDate());
+            Label lbRate = new Label(" Stars= " + f.getRate());
+            SpanLabel lbSeparator = new SpanLabel(" \n ");
+
+            cnt1.addAll(lbId, lbDesc, lbDate, lbRate, lbSeparator);
+
+            Container cnt2 = new Container(BoxLayout.x());
+            Button btnRemoveFeedback = new Button("Remove Feedback");
+            Button btnFindFeedback = new Button("One Feedback");
+
+            cnt2.addAll(btnFindFeedback, btnRemoveFeedback);
+
+            btnFindFeedback.addActionListener(e -> new FindFeedbackForm(current, f).show());
+
+            btnRemoveFeedback.addActionListener((e) -> {
+                try {
+                    Feedback fb = new Feedback(f.getId());
+                    if (ServiceFeedback.getInstance().RemoveFeedback(f)) {
+                        Dialog.show("Success", "Feedback avec ID= " + f.getId() + " a ete supprimee avec succees", new Command("OK"));
+                        new ListFeedbackForm(current).show();
+                    } else {
+                        Dialog.show("ERROR", "Server error", new Command("OK"));
+                    }
+                } catch (NumberFormatException err) {
+                    Dialog.show("ERROR", "Status must be a number", new Command("OK"));
+                }
+            });
+
+            addAll(cnt2, cnt1);
+        }
+        if (previous.getTitle() == "Home") {
+            getToolbar().addMaterialCommandToLeftBar("Home", FontImage.MATERIAL_ARROW_BACK, e -> previous.showBack());
+        } else {
+            getToolbar().addMaterialCommandToLeftBar("Home", FontImage.MATERIAL_ARROW_DROP_UP, e -> new HomeForm().show());
+            //getToolbar().addMaterialCommandToLeftBar(previous.getTitle(), FontImage.MATERIAL_ARROW_BACK,e -> previous.showBack());
+
+        }
+
+    }
+}
---
+++ SPRINT MOBILE/Salem Branch/0.3 User Login/UserController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use MainBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
+/**
+ * User controller.
+ *
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all user entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('MainBundle:User')->findAll();
+
+        return $this->render('user/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+    public function changeEtatAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('MainBundle:User')->findById($id);
+        if($user[0]->isEnabled()==true)
+        {$user[0]->setEnabled(false);
+
+        }
+        else{$user[0]->setEnabled(true);}
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user[0]);
+        $em->flush();
+        return $this->redirectToRoute('user_index');
+    }
+
+    /**
+     * Finds and displays a user entity.
+     *
+     */
+    public function showAction(User $user)
+    {
+
+        return $this->render('user/show.html.twig', array(
+            'user' => $user,
+        ));
+    }
+
+
+    public function allAction(Request $request)
+    {
+        $u=null;
+
+        $users = $this->getDoctrine()->getManager()
+            ->getRepository('MainBundle:User')
+            ->findAll();
+
+
+        foreach ($users as $user)
+        {
+            if($user->getUsernameCanonical()==$request->get('username'))
+            {
+                $u=$user;
+            }
+        }
+        $normalizer = new ObjectNormalizer(null, null, null, new ReflectionExtractor());
+        $serializer = new Serializer([new DateTimeNormalizer(), $normalizer]);
+
+        //$formatted = $serializer->normalize($u);
+        $formatted = $serializer->normalize($u,'json', [AbstractNormalizer::ATTRIBUTES => ['id','firstname','lastname','phone','date','image','username','email','password','lastLogin','roles']]);
+
+
+        return new JsonResponse($formatted);
+
+    }
+
+
+
+
+}
