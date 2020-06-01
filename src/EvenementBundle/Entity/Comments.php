b10049d47a47173f457922b011490708dea4a5da
LQss11 <49565791+LQss11@users.noreply.github.com>
2020-06-01 22:32:23 +0200

    test

---
+++ src/EvenementBundle/Entity/Comments.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace EvenementBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comments
+ *
+ * @ORM\Table(name="comments")
+ * @ORM\Entity(repositoryClass="EvenementBundle\Repository\CommentsRepository")
+ */
+class Comments
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="idevent", type="integer")
+     */
+    private $idevent;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comment", type="string", length=255)
+     */
+    private $comment;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idevent
+     *
+     * @param integer $idevent
+     *
+     * @return Comments
+     */
+    public function setIdevent($idevent)
+    {
+        $this->idevent = $idevent;
+
+        return $this;
+    }
+
+    /**
+     * Get idevent
+     *
+     * @return int
+     */
+    public function getIdevent()
+    {
+        return $this->idevent;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return Comments
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
+
